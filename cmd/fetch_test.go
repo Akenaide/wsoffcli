597137c16fd9cc1df1c7a23c9de7724880dde9e5
Akenaide <kevinms1@gmail.com>
2023-10-03 10:09:34 +0200

    -s expansion is broken #27
    Add --recent

--- cmd/fetch_test.go
+++ cmd/fetch_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 	"os"
+	"slices"
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
@@ -22,5 +23,40 @@ func TestGetLastPage(t *testing.T) {
 	last := getLastPage(doc)
 	if last != 69 {
 		t.Errorf("%v is not last", last)
+	}
+}
+
+func TestRecentSwitch(t *testing.T) {
+	expectedExpension := []string{
+		"444",
+		"439",
+		"443",
+		"442",
+		"438",
+		"437",
+		"441",
+		"440",
+	}
+	f, err := os.Open("mockws/recent.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	recentFurnitures := getRecentFurnitures(doc)
+	if len(recentFurnitures) != 8 {
+		t.Errorf("Should be equal to 8: %v", recentFurnitures)
+	}
+
+	for _, furni := range recentFurnitures {
+		expansion := furni.Values.Get("expansion")
+		if slices.Contains(expectedExpension, expansion) == false {
+			t.Errorf("Did not expect %v expansion", expansion)
+		}
+
 	}
 }
